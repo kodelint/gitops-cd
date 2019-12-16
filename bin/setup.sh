7db3da454c304f377fd27f3809c4e43a89b40f9f
Satyajit Roy <satyajit@adobe.com>
2019-12-15 16:25:08 -0800

    Add a hacky script

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,7 +3,7 @@
 if [[ "$1" == 'create' ]]; then
   for app in `ls -l ./elastic/environments/ | awk '{print $9}'`
    do
-    argocd app $1 $app --project hackathon --repo https://github.com/kodelint/gitops-cd.git --path ./elastic/environments/$app --dest-server https://kubernetes.default.svc --dest-namespace es
+    argocd app $1 $app --project hackathon --repo https://github.com/kodelint/gitops-cd.git --path ./elastic/environments/$app --dest-server https://kubernetes.default.svc --dest-namespace es --sync-policy automated --self-heal --auto-prune --directory-recurse
   done
 fi
 
