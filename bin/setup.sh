ea53cd837bd05058e77318f3c10a7762783ad8be
Satyajit Roy <satyajit@adobe.com>
2019-12-15 16:14:09 -0800

    Add a hacky script

---
+++ bin/setup.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+if [[ "$1" == 'create' ]]; then
+  for app in `ls -l ./elastic/environments/ | awk '{print $9}'`
+   do
+    argocd app $1 $app --project hackathon --repo https://github.com/kodelint/gitops-cd.git --path ./elastic/environments/$app --dest-server https://kubernetes.default.svc --dest-namespace es
+  done
+fi
+
+if [[ "$1" == 'delete' ]]; then
+  for app in `ls -l ./elastic/environments/ | awk '{print $9}'`
+   do
+    argocd app $1 $app
+  done
+fi
